7628901a02acb8d69c46c7cfa12121df44bc5a83
TheDevEnv <thedevenv@gmail.com>
2020-04-25 00:03:13 +0530

    comp: notification component

---
+++ components/notification.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="isLoading" class="dialog">
+    <div>
+      <p>Please wait...</p>
+      <div class="loader"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters(['isLoading'])
+  }
+}
+</script>
+
+<style>
+  .loader {
+    width: 100%;
+    height: 4px;
+    background: green;
+  }
+</style>
