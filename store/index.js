6daad9a6e8f3379d2c9762041f0eb5901629fa5d
TheDevEnv <thedevenv@gmail.com>
2020-04-28 10:43:50 +0530

    refator: currentFetch added in store for global access

--- store/index.js
+++ store/index.js
@@ -1,19 +1,27 @@
 export const state = () => ({
-  isLoading: false
+  isLoading: false,
+  currentFetch: 'india'
 })
 
 export const getters = {
-  isLoading: state => state.isLoading
+  isLoading: state => state.isLoading,
+  currentFetch: state => state.currentFetch
 }
 
 export const mutations = {
   UPDATE_LOADER(state, data) {
     state.isLoading = data
+  },
+  UPDATE_CURRENT_FETCH(state, data) {
+    state.currentFetch = data
   }
 }
 
 export const actions = {
   updateLoadingStatus({ commit }, data) {
     commit('UPDATE_LOADER', data);
+  },
+  updateCurrentFetch({ commit }, data) {
+    commit('UPDATE_CURRENT_FETCH', data);
   }
 }
